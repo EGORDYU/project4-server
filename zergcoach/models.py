a67aeb78ed05e69d1ae668f590feb6fbcc93fa6c
EGORDYU <egordyuzhev@gmail.com>
2023-05-24 00:27:50 -0600

    BIG PROGRESS

--- zergcoach/models.py
+++ zergcoach/models.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.models import User
 from django.db import models
+from markdown_it import MarkdownIt
+
+md = MarkdownIt()
+
 
 class BuildOrder(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
-    buildorder = models.TextField(default="")  # Add a default value here
-    matchup = models.CharField(max_length=120, default="")  # And here
+    buildorder = models.TextField(default="")
+    matchup = models.CharField(max_length=120, default="")
+    imgur_link = models.CharField(max_length=255, null=True, blank=True)  # Add imgur_link field
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.buildorder = md.render(self.buildorder)  # Format buildorder content as Markdown
+        super().save(*args, **kwargs)
 
 
 
--- zergcoach/serializers.py
+++ zergcoach/serializers.py
@@ -7,7 +7,7 @@ import logging
 class BuildOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = BuildOrder
-        fields = ('id', 'title', 'description', 'buildorder','matchup')
+        fields = ('id', 'title', 'description', 'buildorder','matchup', 'imgur_link')
 
 class CommentSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='user.username')
