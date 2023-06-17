d31de631175e2920bd065a3bf89b4c06e9ab84ac
EGORDYU <egordyuzhev@gmail.com>
2023-06-17 16:35:27 -0600

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -156,4 +156,3 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 
-CORS_ALLOW_CREDENTIALS = True
