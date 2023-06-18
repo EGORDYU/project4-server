b0d4bb78bcf3637d5b030cd5c1f09e74c30ea0a6
EGORDYU <egordyuzhev@gmail.com>
2023-06-17 18:16:27 -0600

    profile

--- backend/urls.py
+++ backend/urls.py
@@ -28,6 +28,7 @@ router.register(r'comments', views.CommentView, basename='comment')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/user/profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('zergcoach.urls')),
