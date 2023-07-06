3904bec73689697832e7eef849f9399bdd371b94
EGORDYU <egordyuzhev@gmail.com>
2023-07-06 04:13:24 -0600

    deploy

--- backend/settings.py
+++ backend/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'rest_framework_simplejwt.authentication.JWTAuthentication',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
